e9d54874c7851980c8f14600c672ea86086cbb27
johanmuslim <64147870+johanmuslim@users.noreply.github.com>
2024-06-28 11:18:01 +0700

    Update streamlit_app.py
    menampilkan dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 # %%writefile  webapp.py 
-import streamlit as st 
+import streamlit as st
+import pandas as pd 
+import requests
+from st_aggrid import AgGrid
+
+#baca dataframe dari file csv 
+house = pd.read_csv('house_clean.csv')
 
 def main() : 
   st.header('Halaman Streamlit Johan M.')
@@ -7,6 +13,14 @@ def main() :
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
+  
+  st.write('Contoh dataframe')
+  st.dataframe(house)
+  st.write('Metrics')
+  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  AgGrid(house)
+  st.table([x for x in range(1,5)])
 
 if __name__ == '__main__' : 
   main()
